3a1f14978674b35622815348260694c25f54ed61
Soumya Biswas <soumyabiswas250498@gmail.com>
2025-02-20 23:00:18 +0530

    offline msg issue fixed

--- src/socket/test.js
+++ src/socket/test.js
@@ -1,37 +1,42 @@
 import { jwtAuth } from "../middlewares/userAuthMiddleware.js";
-import { saveOfflineMessage, removeOfflineMessage } from "../services/deviceServices.js";
+import {
+  saveOfflineMessage,
+  removeOfflineMessage,
+} from "../services/deviceServices.js";
 
 const handleSocketConnection = (io) => {
-  const deviceSocketMap = {}; // { deviceId: socketId }
-  const userDeviceMap = {}; // { userId: [deviceId1, deviceId2] }
+  const deviceSocketMap = {};
+  const userDeviceMap = {};
 
   io.use(async (socket, next) => {
     try {
       const { deviceid, authorization } = socket.handshake.headers;
       const userData = await jwtAuth(authorization);
       const userId = userData._id.toString();
-
       let offlineMsg = null;
       const deviceIds = userData.deviceIds.map((item) => {
         const deviceIdCurrent = item._id.toString();
-        if (deviceIdCurrent === deviceid) offlineMsg = item.messages;
+        if (deviceIdCurrent === deviceid) {
+          offlineMsg = item.messages;
+        }
         return deviceIdCurrent;
       });
+      const isValidDevice = userId && deviceIds.includes(deviceid);
 
-      if (!deviceIds.includes(deviceid)) {
+      if (isValidDevice) {
+        deviceSocketMap[deviceid] = socket.id;
+        userDeviceMap[userId] = deviceIds;
+        // console.log(deviceSocketMap, userDeviceMap, '***');
+        socket.deviceId = deviceid;
+        socket.userId = userId;
+        socket.offlineMsg = offlineMsg;
+
+        next();
+      } else {
         return next(new Error("Unauthorized Access"));
       }
-
-      deviceSocketMap[deviceid] = socket.id;
-      userDeviceMap[userId] = deviceIds;
-
-      socket.deviceId = deviceid;
-      socket.userId = userId;
-      socket.offlineMsg = offlineMsg;
-
-      next();
     } catch (error) {
-      console.error("Socket Auth Error:", error.message);
+      console.log(error);
       return next(new Error("Unauthorized Access"));
     }
   });
@@ -39,33 +44,42 @@ const handleSocketConnection = (io) => {
   io.on("connection", (socket) => {
     const { deviceId, userId, offlineMsg } = socket;
 
-    if (offlineMsg?.length) {
-      io.to(socket.id).emit("offlineMsg", offlineMsg);
-      removeOfflineMessage(deviceId);
-    }
+    // console.log("User connected", socket.handshake.headers);
+    // console.log("Id", socket.id);
+    // console.log(deviceId, '***')
+    // console.log(socket, '***');
+
+    socket.on("getOfflineMsg", () => {
+      if (offlineMsg.length) {
+        const toSocketId = deviceSocketMap[deviceId];
+        console.log(toSocketId, deviceId, offlineMsg, "***t");
+        io.to(toSocketId).emit("offlineMsg", offlineMsg);
+        removeOfflineMessage(deviceId);
+      }
+    });
 
     socket.on("c2s_message", async ({ toDeviceId, message }) => {
-      console.log(userDeviceMap, '***')
-      if (!userDeviceMap[userId]?.includes(toDeviceId)) {
-        console.warn("Unauthorized message attempt");
-        socket.emit("error", "Unauthorized Access");
-        return;
+      // console.log(deviceId, toDeviceId, message, '***c2s');
+      const isReceiverAllowed = userDeviceMap[userId].includes(toDeviceId);
+
+      if (!isReceiverAllowed) {
+        console.log("Unauthorized Message");
+        return new Error("Unauthorized Access");
       }
 
       const targetSocketId = deviceSocketMap[toDeviceId];
       if (targetSocketId) {
         io.to(targetSocketId).emit("s2c_message", { from: deviceId, message });
       } else {
-        console.log(`Device ${toDeviceId} is offline. Storing message.`);
+        console.log("receiver is not online");
         saveOfflineMessage(toDeviceId, message, deviceId);
       }
     });
 
-    socket.on("disconnect", () => {
-
+    socket.on("disconnect", (reason) => {
       delete deviceSocketMap[deviceId];
-      console.log(deviceSocketMap, '***')
-
+      console.log(reason, "***");
+      // console.log(deviceSocketMap, "***");
     });
   });
 };
